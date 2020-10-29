a4b05bd667be5f4cb80814fae69255de834f3df9
airotod <airotod@gmail.com>
2020-10-29 11:25:23 +0900

    Implement tab bar on the top in my coupon page

--- src/customer/mycoupon.js
+++ src/customer/mycoupon.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import TopBar from '../components/topbar';
-import { BLACK_COLOR } from '../models/colors';
+import {
+  GREY_20_COLOR,
+  GREY_60_COLOR,
+  RED_COLOR,
+  WHITE_COLOR,
+} from '../models/colors';
+
+import MyCouponLog from './mycoupontab/mycouponlog';
+import MyCoupons from './mycoupontab/mycoupons';
+
+const Tab = createMaterialTopTabNavigator();
 
 const MyCoupon = ({ route, navigation }) => {
   return (
@@ -13,22 +23,34 @@ const MyCoupon = ({ route, navigation }) => {
         drawerShown={true}
         myaccountShown={true}
       />
-      <View style={styles.container}>
-        <Text style={styles.mainText}>My 쿠폰 화면</Text>
-      </View>
+      <Tab.Navigator
+        initialRouteName="보유 쿠폰"
+        tabBarOptions={{
+          activeTintColor: RED_COLOR,
+          inactiveTintColor: GREY_60_COLOR,
+          indicatorStyle: { backgroundColor: RED_COLOR },
+          pressColor: GREY_20_COLOR,
+          labelStyle: { fontSize: 16 },
+          style: { backgroundColor: WHITE_COLOR, height: 44 },
+          tabStyle: {
+            borderBottomColor: GREY_20_COLOR,
+            borderBottomWidth: 1,
+            height: 44,
+          },
+        }}>
+        <Tab.Screen
+          name="보유 쿠폰"
+          component={MyCoupons}
+          options={{ tabBarLabel: '보유 쿠폰' }}
+        />
+        <Tab.Screen
+          name="쿠폰 내역"
+          component={MyCouponLog}
+          options={{ tabBarLabel: '쿠폰 내역' }}
+        />
+      </Tab.Navigator>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  mainText: {
-    color: BLACK_COLOR,
-  },
-});
 
 export default MyCoupon;
