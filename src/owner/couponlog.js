489996f21a5152a8c4aa38f28aa15715e94ca998
Baek <b970311@yonsei.ac.kr>
2020-10-30 16:02:14 +0900

    for merge

--- src/owner/couponlog.js
+++ src/owner/couponlog.js
@@ -1,27 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-import { BLACK_COLOR } from '../models/colors';
+import TopBar from '../components/topbar';
+import {
+  GREY_20_COLOR,
+  GREY_60_COLOR,
+  RED_COLOR,
+  WHITE_COLOR,
+} from '../models/colors';
 
-const CouponLog = () => {
+import CouponLogRecord from './couponlogtab/couponlogrecord';
+import CouponLogStat from './couponlogtab/couponlogstat';
+
+const Tab = createMaterialTopTabNavigator();
+
+const CouponLog = ({ route, navigation }) => {
   return (
     <>
-      <View style={styles.container}>
-        <Text style={styles.mainText}>쿠폰 사용량 화면</Text>
-      </View>
+      <TopBar
+        title="쿠폰 사용량"
+        navigation={navigation}
+        drawerShown={true}
+        myaccountShown={true}
+        myaccountColor={GREY_60_COLOR}
+      />
+      <Tab.Navigator
+        initialRouteName="쿠폰 사용량 기록"
+        tabBarOptions={{
+          activeTintColor: RED_COLOR,
+          inactiveTintColor: GREY_60_COLOR,
+          indicatorStyle: { backgroundColor: RED_COLOR },
+          pressColor: GREY_20_COLOR,
+          labelStyle: { fontSize: 16 },
+          style: { backgroundColor: WHITE_COLOR, height: 44 },
+          tabStyle: {
+            borderBottomColor: GREY_20_COLOR,
+            borderBottomWidth: 1,
+            height: 44,
+          },
+        }}>
+        <Tab.Screen
+          name="쿠폰 사용량 기록"
+          component={CouponLogRecord}
+          options={{ tabBarLabel: '기록' }}
+        />
+        <Tab.Screen
+          name="쿠폰 사용량 통계"
+          component={CouponLogStat}
+          options={{ tabBarLabel: '통계' }}
+        />
+      </Tab.Navigator>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  mainText: {
-    color: BLACK_COLOR,
-  },
-});
 
 export default CouponLog;
