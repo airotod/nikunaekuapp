cb22dd5adc7022ce3a6030752ddea4877f429302
2hwayoung <hwa3270@gmail.com>
2020-11-08 23:03:00 +0900

    Implement test page for uploading json data into firestore

--- src/customer/mypointtab/mypointlog.js
+++ src/customer/mypointtab/mypointlog.js
@@ -1,13 +1,86 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { FlatList, StyleSheet, Text, View, Image} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import firestore from '@react-native-firebase/firestore';
 
-import { BLACK_COLOR } from '../../models/colors';
+import { 
+  BLACK_COLOR,
+  WHITE_COLOR, 
+} from '../../models/colors';
+
+const PointLogItem = ({ is_usage, date, point, logtitle, detail, balance  }) => {
+  return (
+    <View style={styles.listcontainer}>
+      <View style={styles.itemcontainer}>
+        <Image
+          style={styles.imageItem}
+          source={require('./mypoint_images/use_image.png')} />
+        <Text style={styles.pointText}>{point}</Text>
+      </View>
+      <View style={styles.linecontainer}>
+        <Image
+          style={styles.lineItem}
+          source={require('./mypoint_images/line_image.png')} />
+      </View>
+      <View style={styles.detailcontainer}>
+        <Text style={styles.dateText}>{date}</Text>
+        <Text style={styles.logtitleText}>{logtitle}</Text>
+        <Text style={styles.detailText}>{detail}</Text>
+      </View>
+      <View style={styles.balancecontainer}>
+        <Text style={styles.balanceText}>{balance}</Text>
+      </View>
+  </View>
+  );
+};
 
 const MyPointLog = ({ route, navigation }) => {
+  const [pointLogList, setPointLogList] = useState([]);
+  const [selectedValue, setSelectedValue] = useState("전체");
+  
+  const ref = firestore().collection('client').doc('nGnPbG8xIg7onVaowgyr').collection('point_log');
+
+  useEffect(() => {
+    return ref.onSnapshot((querySnapshot) => {
+      let items = [];
+      querySnapshot.forEach((doc) => {
+        const { is_usage, date, point, logtitle, detail, balance } = doc.data();
+        items.push({
+          is_usage: is_usage,
+          date: date,
+          point: point,
+          logtitle: logtitle,
+          detail: detail,
+          balance: balance
+        });
+      });
+      setPointLogList(items);
+    });
+  }, []);
+
   return (
     <>
       <View style={styles.container}>
-        <Text style={styles.mainText}>포인트 적립 및 사용 내역 화면</Text>
+        <View style={styles.selectcontainer}>
+          <Picker 
+            selectedValue={selectedValue}
+            style={styles.pickerstyle}
+            onValueChange={(itemValue, itemIndex) => 
+              setSelectedValue(itemValue)
+            }>
+            <Picker.Item label='전체' value='전체' />
+            <Picker.Item label='적립' value='적립' />
+            <Picker.Item label='사용' value='사용' />
+            
+          </Picker>
+        </View>
+        <View style={styles.maincontainer}>
+          <FlatList
+            data={pointLogList}
+            renderItem={({ item }) => <PointLogItem {...item} />}
+            keyExtractor={(item) => {item.is_usage, item.date, item.point, item.logtitle, item.detail, item.balance }}
+          />
+        </View>
       </View>
     </>
   );
@@ -16,12 +89,75 @@ const MyPointLog = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 10
+  },
+  selectcontainer: {
+    padding: 5,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  pickerstyle:{
+    height: 40,
+    width: 100,
+  },
+  maincontainer: {
+    flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
   },
-  mainText: {
-    color: BLACK_COLOR,
+  listcontainer: {
+    height: 100,
+    width: 380,
+    flexDirection: 'row',
+    marginBottom: 2,
+    backgroundColor: WHITE_COLOR,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  itemcontainer: {
+    flex: 3,
+    flexDirection: 'column',
+    margin: 1,
+    alignItems: 'center',
+  },
+  imageItem: { 
+    height: 30,
+    width: 30,
+    margin: 2,
+  },
+  pointText: {
+    margin: 5,
+    fontSize: 18,
+  },
+  linecontainer: {
+    flex: 1
+  },
+  lineItem: {
+    height: 70,
+    width: 2,
+    margin: 2,
+  },
+  detailcontainer: {
+    flex: 6,
+  },
+  dateText: {
+    
+    margin: 3,
+  },
+  logtitleText: {
+    fontSize: 18,
+    margin: 3,
+  },
+  detailText: {
+    
+    margin: 3,
+  },
+  balancecontainer: {
+    flex: 2,
+  },
+  balanceText: {
+
+  }
 });
 
 export default MyPointLog;
--- src/customer/mypointtab/mywallet.js
+++ src/customer/mypointtab/mywallet.js
@@ -1,13 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
-import { BLACK_COLOR } from '../../models/colors';
+import { 
+  GREY_10_COLOR,
+  BLACK_COLOR,
+  WHITE_COLOR,
+  YELLO_COLOR,
+  YELLO_COLOR_BRIGHT 
+} from '../../models/colors';
 
 const MyWallet = ({ route, navigation }) => {
+  const ref = firestore().collection('client');
+  
   return (
     <>
       <View style={styles.container}>
-        <Text style={styles.mainText}>내 지갑 화면</Text>
+        <View style={styles.maincontainer}>
+          <Text style={styles.topText}>보유 포인트</Text>
+          <View style={styles.pointcontainer}>
+            <View style={styles.centercontainer}>
+              <Text style={styles.pointText}>1,200원</Text>
+              <View style={styles.threebuttons}>
+                <Button title={"충천"} color={YELLO_COLOR_BRIGHT}></Button>
+                <Button title={"선물"} color={YELLO_COLOR_BRIGHT}></Button>
+                <Button title={"인출"} color={YELLO_COLOR_BRIGHT}></Button>
+              </View>
+            </View>
+            <View style={styles.detailcontainer}>
+              <View style={styles.detailpoint}>
+                <Text style={styles.detailText}>적립</Text>
+                <Text style={styles.detailValue}>300원</Text>
+              </View>
+              <View style={styles.detailpoint}>
+                <Text style={styles.detailText}>충전</Text>
+                <Text style={styles.detailValue}>2000원</Text>
+              </View>
+              <View style={styles.detailpoint}>
+                <Text style={styles.detailText}>사용</Text>
+                <Text style={styles.detailValue}>0원</Text>
+              </View>
+            </View>
+          </View>
+
+        </View>
+        
       </View>
     </>
   );
@@ -16,12 +53,57 @@ const MyWallet = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 26,
+    alignItems: 'center',
+    backgroundColor: GREY_10_COLOR,
+  },
+  maincontainer:{
+    width: 310,
+    height : 180,
+    padding: 20,
+    backgroundColor: WHITE_COLOR,
+    borderRadius : 15,
+  },
+  topText: {
+    color: BLACK_COLOR,
+    fontSize : 12,
+  },
+  pointcontainer: {
+    flex:1,
+    flexDirection: 'column',
+  },
+  centercontainer: {
+    flex:1,
+    flexDirection: 'row',
     alignItems: 'center',
   },
-  mainText: {
+  pointText: {
+    flex: 1,
     color: BLACK_COLOR,
+    fontSize: 25,
+    fontWeight: 'bold',
   },
+  threebuttons: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  detailcontainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  detailpoint: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  detailText: {
+    color: BLACK_COLOR,
+    fontSize: 12,
+  },
+  detailValue: {
+    color: BLACK_COLOR,
+    fontSize: 15,
+  }
 });
 
 export default MyWallet;
