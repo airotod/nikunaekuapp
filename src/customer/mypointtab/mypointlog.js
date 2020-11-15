9b34d3e82ca38e5b1926897ddaf654feb5f95372
2hwayoung <hwa3270@gmail.com>
2020-11-16 02:37:29 +0900

    up to date

--- src/customer/mypointtab/mypointlog.js
+++ src/customer/mypointtab/mypointlog.js
@@ -1,13 +1,130 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { FlatList, StyleSheet, Text, View, Image, Picker} from 'react-native';
+// import {Picker} from '@react-native-picker/picker';
+import firestore from '@react-native-firebase/firestore';
 
-import { BLACK_COLOR } from '../../models/colors';
+import { 
+  BLACK_COLOR,
+  WHITE_COLOR, 
+  RED_COLOR,
+  GREY_70_COLOR,
+  GREEN_COLOR
+} from '../../models/colors';
+import { and } from 'react-native-reanimated';
+
+const PointLogItem = ({is_usage, date, point, logtitle, detail, balance  }) => {
+  let IconItem = null;
+  let dateItems = date.split(" ");
+
+  if (is_usage) {
+    IconItem = <View style={styles.itemcontainer}>
+                  <Image
+                  style={styles.imageItem}
+                  source={require('./mypoint_images/use_image.png')} />
+                  <Text style={styles.pointText_use}>{point}원</Text>
+                </View>;
+  } else {
+    IconItem = <View style={styles.itemcontainer}>
+                  <Image
+                  style={styles.imageItem}
+                  source={require('./mypoint_images/reward_image.png')} />
+                  <Text style={styles.pointText_reward}>+{point}원</Text>
+                </View>;};
+
+  return (
+      <View style={styles.listcontainer}>     
+        {IconItem}      
+        <View style={styles.linecontainer}>
+          <Image
+            style={styles.lineItem}
+            source={require('./mypoint_images/line_image.png')} />
+        </View>
+        <View style={styles.contextcontainer}>
+          <View style={styles.detailcontainer}>
+            <Text style={styles.dateText}>{dateItems[3]}년 {dateItems[1]}월 {dateItems[2]}일 {dateItems[0]}요일</Text>
+            <Text style={styles.logtitleText}>{logtitle}</Text>
+          </View>
+          <View style={styles.balancecontainer}>
+            <Text style={styles.detailText}>{detail}</Text>
+            <Text style={styles.balanceText}>잔액 {balance}원</Text>
+          </View>
+        </View>
+      </View>
+  );
+};
 
 const MyPointLog = ({ route, navigation }) => {
+  const [pointLogList, setPointLogList] = useState([]);
+  const [selectedValue, setSelectedValue] = useState("전체");
+  const ref = firestore().collection('client').where('clientID','==','yonsei4').orderBy('date', 'desc');
+  
+  useEffect(() => {
+    return ref.onSnapshot((querySnapshot) => {
+      let items = [];
+      querySnapshot.forEach((doc) => {
+        const { is_usage, date, point, logtitle, detail, balance } = doc.data();
+        if ((selectedValue) == "전체") {
+          items.push({
+            is_usage: is_usage,
+            date: date.toDate().toString(),
+            point: point,
+            logtitle: logtitle,
+            detail: detail,
+            balance: balance,
+          });
+        }
+        else if ((selectedValue) == "적립"){
+          if(!(is_usage)){
+            items.push({
+              is_usage: is_usage,
+              date: date.toDate().toString(),
+              point: point,
+              logtitle: logtitle,
+              detail: detail,
+              balance: balance,
+            });
+          }
+        }
+        else if ((selectedValue) == "사용"){
+          if((is_usage)){
+            items.push({
+              is_usage: is_usage,
+              date: date.toDate().toString(),
+              point: point,
+              logtitle: logtitle,
+              detail: detail,
+              balance: balance,
+            });
+          }
+        }
+       });
+      setPointLogList(items);
+      console.log(selectedValue);
+    });
+  }, [selectedValue]);
+
   return (
     <>
       <View style={styles.container}>
-        <Text style={styles.mainText}>포인트 적립 및 사용 내역 화면</Text>
+        <View style={styles.selectcontainer}>
+          {/* <Picker 
+            mode = 'dropdown'
+            selectedValue={selectedValue}
+            style={styles.pickerstyle}
+            onValueChange={(itemValue, itemIndex) => {
+              setSelectedValue(itemValue)              
+            }}>
+            <Picker.Item label='전체' value='전체'/>
+            <Picker.Item label='적립' value='적립'/>
+            <Picker.Item label='사용' value='사용'/>
+          </Picker> */}
+        </View>
+        <View style={styles.maincontainer}>
+          <FlatList
+            data={pointLogList}
+            renderItem={({ item }) => <PointLogItem {...item} />}
+          />
+        </View>
       </View>
     </>
   );
@@ -16,12 +133,95 @@ const MyPointLog = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10
+  },
+  selectcontainer: {
+    padding: 5,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  pickerstyle:{
+    height: 40,
+    width: 100,
+  },
+  maincontainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  listcontainer: {
+    height: 100,
+    width: 400,
+    flexDirection: 'row',
+    marginBottom: 2,
+    backgroundColor: WHITE_COLOR,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  itemcontainer: {
+    height: 100,
+    width: 100,
+    flexDirection: 'column',
+    margin: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  mainText: {
-    color: BLACK_COLOR,
+  imageItem: { 
+    height: 40,
+    width: 40,
+    margin: 2,
   },
+  pointText_use: {
+    margin: 5,
+    fontSize: 16,
+    color: RED_COLOR,
+  },
+  pointText_reward: {
+    margin: 5,
+    fontSize: 16,
+    color: GREEN_COLOR,
+  },
+  linecontainer: {
+    height: 100,
+    width: 20,
+    justifyContent: 'center',
+  },
+  lineItem: {
+    height: 70,
+    width: 2,
+    margin: 2,
+  },
+  contextcontainer: {
+    height: 100,
+    width: 280,
+    flexDirection: 'column',
+  },
+  detailcontainer: {
+    flex: 1,
+    flexDirection: 'column',
+    marginVertical: 10,
+    justifyContent: 'center'
+  },
+  dateText: {
+    color: GREY_70_COLOR,
+    marginVertical: 8,
+  },
+  logtitleText: {
+    fontSize: 16,
+  },
+  balancecontainer: {
+    flex: 1,
+    marginRight: 12,
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  detailText: {
+    color: GREY_70_COLOR,
+  },
+  balanceText: {
+    color: GREY_70_COLOR,
+  }
 });
 
 export default MyPointLog;
--- src/customer/mypointtab/mywallet.js
+++ src/customer/mypointtab/mywallet.js
@@ -1,14 +1,263 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState}  from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert, Modal, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
-import { BLACK_COLOR } from '../../models/colors';
+import { 
+  GREY_10_COLOR,
+  BLACK_COLOR,
+  WHITE_COLOR,
+  YELLO_COLOR,
+  GREY_40_COLOR,
+  YELLO_COLOR_BRIGHT, 
+  RED_COLOR,
+  GREY_90_COLOR,
+  GREY_80_COLOR
+} from '../../models/colors';
+
+const PointItems = ({ point }) => {
+  return (
+    <Text style={styles.pointText}>{point}원</Text>
+  );
+};
 
 const MyWallet = ({ route, navigation }) => {
+  const [totalPoint, setTotalPoint] = useState(null);
+  const [isVisible1, setIsVisible1] = useState(false);
+  const [isVisible2, setIsVisible2] = useState(false);
+  const [isVisible3, setIsVisible3] = useState(false);
+  const [plusPoint, setPlusPoint] = useState(null);
+  const [friendID, setFriendID] = useState(null);
+
+  const ref = firestore().collection('client').where('clientID', '==' ,'hwayoung1');
+
+
+  function _addPointHandler(){
+    Alert.alert('포인트 충전', plusPoint+'(원) 포인트를 충전하시겠습니까?', [
+      {
+        text: '취소',
+        onPresse: () => {},
+      },
+      {
+        text: '충전',
+        onPress: async () => {
+          await ref.update({
+            point: point+plusPoint,
+          });
+          setPlusPoint(null);
+        },
+      },
+    ]);
+  }
+
+  function _givePointHandler(){
+    Alert.alert('포인트 선물', friendID+' 에게 '+plusPoint+'(원) 포인트를 선물하시겠습니까?', [
+      {
+        text: '취소',
+        onPresse: () => {},
+      },
+      {
+        text: '선물',
+        onPress: async () => {
+          await ref.update({
+            point: point-plusPoint,
+          });
+          setPlusPoint(null);
+        },
+      },
+    ]);
+  }
+
+  function _withdrawePointHandler(){
+    Alert.alert('포인트 인출', '정말로 포인트를 인출하시겠습니까?', [
+      {
+        text: '취소',
+        onPresse: () => {},
+      },
+      {
+        text: '인출',
+        onPress: async () => {
+          await ref.update({
+            point: point-plusPoint,
+          });
+          setPlusPoint(null);
+        },
+      },
+    ]);
+  }
+ //useEffect 값 하나면 가져오는 방법!!!
+  // useEffect(() => {
+  //   return ref.onSnapshot((querySnapshot) => {
+  //     let items = [];
+  //     querySnapshot.forEach((doc) => {
+  //       const { totalpoint } = doc.data();
+  //       items.push({
+  //         totalpoint: totalpoint,
+  //       });
+  //     });
+  //       setTotalPoint(items);
+  //   });
+  // }, []);
+
   return (
     <>
       <View style={styles.container}>
-        <Text style={styles.mainText}>내 지갑 화면</Text>
+        <View style={styles.maincontainer}>
+          <View style={styles.topcontainer}>
+            <View style={styles.pointcontainer}>
+              <Text style={styles.topText}>보유 포인트</Text>
+              <PointItems point={1200}/>
+            </View>
+            <View style={styles.buttoncontainer}>  
+              <View style={styles.threebuttons}>
+                <Button title={"충전"} color={YELLO_COLOR_BRIGHT}
+                  onPress={() => {
+                    setIsVisible1(true);}
+                  }>
+                </Button>
+                <Button title={"선물"} color={YELLO_COLOR_BRIGHT}
+                  onPress={() => {
+                    setIsVisible2(true);}
+                  }>
+                </Button>
+                <Button title={"인출"} color={YELLO_COLOR_BRIGHT}
+                  onPress={() => {
+                    setIsVisible3(true);}
+                  }>
+                </Button>
+              </View> 
+            </View>
+          </View>
+          <View>
+          </View>
+          <View style={styles.detailcontainer}>
+            <View style={styles.detailpoint}>
+              <Text style={styles.detailText}>적립</Text>
+              <Text style={styles.detailValue}>300원</Text>
+            </View>
+            <View style={styles.detailpoint}>
+              <Text style={styles.detailText}>충전</Text>
+              <Text style={styles.detailValue}>2000원</Text>
+            </View>
+            <View style={styles.detailpoint}>
+              <Text style={styles.detailText}>사용</Text>
+              <Text style={styles.detailValue}>0원</Text>
+            </View>
+          </View>
+        </View>
       </View>
+
+      {/* add Point Button */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isVisible1}>
+        <KeyboardAvoidingView behavior="position">
+          <View style={styles.modalView}>
+            <View style={styles.modalTopView}>
+              <Text style={styles.modalText}>충전</Text>
+            </View>
+            <View style={styles.modalMidView}>
+              <TextInput
+                style={styles.input}
+                placeholder="충전금액(원)"
+                onChangeText={(text) => {
+                  setPlusPoint(plusPoint);
+                  console.log(plusPoint);
+                }}
+                value={plusPoint}
+                clearButtonMode="always"
+                autoFocus={true}
+              />
+            </View>
+            <View style={styles.modalBottomView}>
+              <TouchableOpacity style={styles.button1} onPress={_addPointHandler}>
+                <Text style={styles.buttonText}>충전</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button2} onPress={() => {setIsVisible1(false);}}>
+                <Text style={styles.buttonText}>취소</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </Modal>
+
+      {/* give Point Button */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isVisible2}>
+        <KeyboardAvoidingView behavior="position">
+          <View style={styles.modalView2}>
+            <View style={styles.modalTopView2}>
+              <Text style={styles.modalText}>선물</Text>
+            </View>
+            <View style={styles.modalMidView2}>
+              <TextInput
+                style={styles.input}
+                placeholder="선물받을 아이디(id)"
+                onChangeText={(text) => {
+                  setFriendID(friendID)
+                }}
+                value={friendID}
+                clearButtonMode="always"
+                autoFocus={true}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="선물할 금액(원)"
+                onChangeText={(text) => {
+                  setPlusPoint(plusPoint);
+                }}
+                value={plusPoint}
+                clearButtonMode="always"
+                autoFocus={true}
+              />
+            </View>
+            <View style={styles.modalBottomView2}>
+              <TouchableOpacity style={styles.button1} onPress={_givePointHandler}>
+                <Text style={styles.buttonText}>선물</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button2} onPress={() => {setIsVisible2(false);}}>
+                <Text style={styles.buttonText}>취소</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </Modal>
+
+      {/* withdraw Point Button */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isVisible3}>
+        <KeyboardAvoidingView behavior="position">
+          <View style={styles.modalView}>
+            <View style={styles.modalTopView}>
+              <Text style={styles.modalText}>내 계좌로 인출</Text>
+            </View>
+            <View style={styles.modalMidView}>
+              <TextInput
+                style={styles.input}
+                placeholder="인출할 금액(원)"
+                onChangeText={(text) => {
+                  setPlusPoint(points);
+                }}
+                value={plusPoint}
+                clearButtonMode="always"
+                autoFocus={true}
+              />
+            </View>
+            <View style={styles.modalBottomView}>
+              <TouchableOpacity style={styles.button1} onPress={_withdrawePointHandler}>
+                <Text style={styles.buttonText}>인출</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button2} onPress={() => {setIsVisible3(false);}}>
+                <Text style={styles.buttonText}>취소</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </Modal>
     </>
   );
 };
@@ -16,12 +265,166 @@ const MyWallet = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 26,
+    alignItems: 'center',
+    backgroundColor: GREY_10_COLOR,
+  },
+  maincontainer:{
+    width: 310,
+    height : 165,
+    padding: 20,
+    flexDirection: 'column',
+    backgroundColor: WHITE_COLOR,
+    borderRadius : 15,
+  },
+  topcontainer: {
+    flex:1,
+    flexDirection: 'row',
+  },
+  pointcontainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  topText: {
+    flex: 1,
+    color: BLACK_COLOR,
+    fontSize : 12,
+  },
+  pointText: {
+    flex: 1,
+    color: BLACK_COLOR,
+    fontSize: 25,
+    fontWeight: 'bold',
+  },  
+  buttoncontainer: {
+    flex:1,
+    flexDirection: 'row',
     alignItems: 'center',
   },
-  mainText: {
-    color: BLACK_COLOR,
+  threebuttons: {
+    flex: 1,
+    flexDirection: 'row',
   },
+  detailcontainer: {
+    flex: 1,
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  detailpoint: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  detailText: {
+    color: BLACK_COLOR,
+    marginTop: 3,
+    fontSize: 12,
+  },
+  detailValue: {
+    color: BLACK_COLOR,
+    marginTop: 3,
+    fontSize: 15,
+  },
+  modalView: {
+    width: 280,
+    height : 200,
+    marginTop: 320,
+    marginLeft: 65,
+    flexDirection: 'column',
+    backgroundColor: "white",
+    justifyContent: 'center',
+    alignItems: "center",
+    borderRadius: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  modalTopView: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  modalMidView: {
+    flex: 1,
+    width: 200,
+  },
+  modalBottomView: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20,
+    marginBottom: 25,
+  },
+  modalView2: {
+    width: 280,
+    height : 250,
+    marginTop: 320,
+    marginLeft: 65,
+    flexDirection: 'column',
+    backgroundColor: "white",
+    justifyContent: 'center',
+    alignItems: "center",
+    borderRadius: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  modalTopView2: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  modalMidView2: {
+    flex: 2,
+    width: 200,
+  },
+  modalBottomView2: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20,
+    marginBottom: 20,
+  },
+  modalText: {
+    flex: 1,
+    textAlign: "center"
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: GREY_40_COLOR,
+    borderRadius: 5,
+    justifyContent: 'center',
+  },
+  button1: {
+    flex: 1,
+    width: 200,
+    marginBottom: 5,
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: RED_COLOR,
+  },
+  button2: {
+    flex: 1,
+    width: 200,
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: GREY_80_COLOR,
+  },
+  buttonText: {
+    color: WHITE_COLOR
+  }
 });
 
 export default MyWallet;
