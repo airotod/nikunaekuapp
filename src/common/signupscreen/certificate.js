7ea8677f6118b13538cdaef8c3c8df2e7142b209
airotod <airotod@gmail.com>
2020-11-25 07:27:49 +0900

    Implement design of certificate page

--- src/common/signupscreen/certificate.js
+++ src/common/signupscreen/certificate.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import StepButton from '../../components/stepbutton';
 
 import TopBar from '../../components/topbar';
-
-import { BLACK_COLOR, GREY_40_COLOR, RED_COLOR } from '../../models/colors';
+import {
+  BLACK_COLOR,
+  GREY_30_COLOR,
+  GREY_40_COLOR,
+  GREY_50_COLOR,
+  RED_COLOR,
+} from '../../models/colors';
 
 export default function Certificate({ route, navigation }) {
   function _handleComplete(event) {
@@ -20,7 +25,16 @@ export default function Certificate({ route, navigation }) {
         barColor={BLACK_COLOR}
       />
       <View style={styles.container}>
-        <Text style={styles.mainText}>증명서 인증 화면</Text>
+        <View style={styles.mainContents}>
+          <Text style={styles.question}>1. 주민등록증을 등록해주세요.</Text>
+          <View style={styles.img}>
+            <Text style={styles.imgAlt}>주민등록증</Text>
+          </View>
+          <Text style={styles.question}>2. 사업자등록증을 등록해주세요.</Text>
+          <View style={styles.img}>
+            <Text style={styles.imgAlt}>사업자등록증</Text>
+          </View>
+        </View>
         <View style={styles.buttonContainer}>
           <StepButton
             text="이전"
@@ -49,7 +63,29 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 25,
   },
-  mainText: {
+  img: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: GREY_30_COLOR,
+    borderRadius: 15,
+    height: 160,
+    justifyContent: 'center',
+    marginTop: 5,
+    marginBottom: 20,
+    width: '90%',
+  },
+  imgAlt: {
+    color: GREY_50_COLOR,
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  mainContents: {
+    flex: 1,
+  },
+  question: {
     color: BLACK_COLOR,
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
