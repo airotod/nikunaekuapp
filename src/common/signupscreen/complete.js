fd06553e433b8c0af06e4da2b355b3802a2ed223
airotod <airotod@gmail.com>
2020-11-26 05:33:00 +0900

    Implement design of sign up completed screen (owner)

--- src/common/signupscreen/complete.js
+++ src/common/signupscreen/complete.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import 'react-native-gesture-handler';
 
 import TopBar from '../../components/topbar';
-
-import { BLACK_COLOR } from '../../models/colors';
+import { BLACK_COLOR, RED_COLOR } from '../../models/colors';
 
 export default function Complete({ route, navigation }) {
   return (
@@ -15,7 +15,16 @@ export default function Complete({ route, navigation }) {
         barColor={BLACK_COLOR}
       />
       <View style={styles.container}>
-        <Text style={styles.mainText}>신청 완료 화면</Text>
+        <Text style={styles.largeText}>신청 완료</Text>
+        <Text style={styles.redText}>신청해주셔서 감사합니다.</Text>
+        <Text style={styles.smallText}>
+          입력하신 정보를 기준으로 심사가 진행됩니다.{'\n'}영업일 기준 5일
+          이내로 결과를 알려드리겠습니다.
+        </Text>
+        <TouchableOpacity style={styles.iconContainer} onPress={() => {}}>
+          <Icon name="home" size={30} color={RED_COLOR} />
+          <Text style={styles.iconText}> 홈으로 이동</Text>
+        </TouchableOpacity>
       </View>
     </>
   );
@@ -28,7 +37,33 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     margin: 25,
   },
-  mainText: {
+  iconContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 100,
+    width: '80%',
+  },
+  iconText: {
     color: BLACK_COLOR,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'right',
+  },
+  largeText: {
+    color: BLACK_COLOR,
+    fontSize: 36,
+    fontWeight: 'bold',
+  },
+  redText: {
+    color: RED_COLOR,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 50,
+  },
+  smallText: {
+    color: BLACK_COLOR,
+    fontSize: 14,
+    fontWeight: 'bold',
   },
 });
